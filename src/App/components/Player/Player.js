b773fb550ffbb658f7bfd4c0f12956905073f8b7
NewRehtse <newrehtse@gmail.com>
2019-04-07 19:36:24 +0200

    Added player working

--- src/App/components/Player/Player.js
+++ src/App/components/Player/Player.js
@@ -1,41 +1,17 @@
 import React, {Component} from 'react';
 import { connect } from 'react-redux';
+import ReactAudioPlayer from 'react-audio-player';
 
 class Player extends Component {
-    constructor(props) {
-        super(props);
-
-        this.onChangeAudio = this.onChangeAudio.bind(this);
-        this.play = this.play.bind(this);
-    }
-    onChangeAudio() {
-        console.log('pues cambio');
-    }
-
-    play(e) {
-        e.preventDefault();
-        console.log('play');
-    }
-
     render() {
-        return <form>
-            <audio id="audio">
-                <source src={this.props.player.src} onChange={this.onChangeAudio}/>
-            </audio>        
+        return <div className="player">
+            <ReactAudioPlayer
+                src={this.props.player.src}
+                autoPlay
+                controls
+                />       
 
-            <div className="row player border border-secondary ">
-
-                <div className="col">
-                    <a href="#back"><i className="fas fa-angle-left"></i></a>
-                </div>
-                <div className="col">
-                    <button href="#play" onClick={this.play}><i className="fas fa-caret-square-right"></i></button>
-                </div>
-                <div className="col">
-                    <a href="#forward"><i className="fas fa-angle-right"></i></a>
-                </div>
-            </div>
-        </form>
+        </div>
     }
 }
 
